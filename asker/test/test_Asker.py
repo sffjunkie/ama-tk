87711e92a335526c6b5439986ad31fe8683c5282
Simon Kennedy <code@sffjunkie.co.uk>
2013-03-18 20:16:01 +0000

    Added test for Question

--- asker/test/test_Asker.py
+++ asker/test/test_Asker.py
@@ -1,16 +1,24 @@
-from asker import Asker, TkAsker
+from asker import Asker, Question
 
 questions = u'{"status": ["the status of the system", "1, 2\\nor 3", "string", 1, ["1","2","3"]]}'
 
-def test_Asker_split_line():
-    asker = Asker()
-    asker.loads(questions)
+def test_Question():
+    q = Question('key', 'label')
+    assert q.key == 'key'
+    assert q.label == 'label'
+    assert q.type == 'str'
+    assert q.default is None
+    assert q.help_text == ''
+    assert q.validator is None
+    assert len(q.depends_on) == 0
 
-def test_TkAsker():
-    asker = TkAsker()
-    asker.loads(questions)
+def test_Asker_dependencies():
+    asker = Asker()
+    d = asker._find_dependencies('{wally}and{dave}')
+    
+    assert d == ['wally', 'dave']
     
 if __name__ == '__main__':
-    test_Asker_split_line()
-    test_TkAsker()
+    test_Question()
+    test_Asker_dependencies()
     
