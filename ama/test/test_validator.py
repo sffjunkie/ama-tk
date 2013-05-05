cccd1a2c902a1bc3a73bad2903670efc8c004ca2
Simon Kennedy <code@sffjunkie.co.uk>
2013-05-05 12:59:13 +0100

    Added test for date validation

--- ama/test/test_validator.py
+++ ama/test/test_validator.py
@@ -14,6 +14,7 @@
 
 import os
 import tempfile
+import datetime
 from ama.validator import Validators
 
 from pytest import raises
@@ -137,6 +138,10 @@ def test_validate_path_contains_and_does_not_contain_fail():
             v(dname)
     os.rmdir(d)
 
+def test_date():
+    v = Validators['date']
+    d = datetime.date(2013, 04, 23)
+    assert v('2013-04-23') == d
 
 if __name__ == '__main__':
     test_validate_str()
@@ -156,4 +161,5 @@ if __name__ == '__main__':
     test_validate_path_does_not_contain_fail()
     test_validate_path_contains_and_does_not_contain()
     test_validate_path_contains_and_does_not_contain_fail()
+    test_date()
     
