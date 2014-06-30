2ca7e4848a4ddae537f30447a679bae4edb7a8d9
Simon Kennedy <code@sffjunkie.co.uk>
2014-06-30 15:36:15 +0100

    Many changes.

--- ama/tk.py
+++ ama/tk.py
@@ -1,20 +1,8 @@
-# Copyright 2009-2013, Simon Kennedy, code@sffjunkie.co.uk
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2009-2014, Simon Kennedy, code@sffjunkie.co.uk
 
 import sys
 import os.path
-from datetime import date, datetime
+from pkgutil import get_data
 from collections import OrderedDict
 
 try:
@@ -37,13 +25,20 @@ try:
 except ImportError:
     import tkFileDialog as filedialog
 
+try:
+    from base64 import encodebytes
+except ImportError:
+    from base64 import encodestring as encodebytes
+
 from ama import Asker, u
-from ama.tk_tooltip import ToolTip
-from ama.tk_date import DateEntry
-from ama.tk_time import TimeEntry
+from tks.tooltip import ToolTip
+from tks.dates import DateEntry
+from tks.times import TimeEntry
+from tks.colors import ColorEntry
+
 
 class TkAsker(Asker):
-    """Displays a Tk window containing the questins to be asked.
+    """Displays a Tk window containing the questions to be asked.
     
     :param title: The title to display
     :type title:     str
@@ -57,26 +52,28 @@ class TkAsker(Asker):
     :type allow_invlaid:  bool
     """
     
-    def __init__(self, title, preamble='', filename='', allow_invalid=True):
-        Asker.__init__(self, filename)
+    def __init__(self, title, preamble='', ds=None, json_string=None, allow_invalid=True):
+        Asker.__init__(self, ds, json_string)
         self._title = title
         self._preamble = preamble
         self._allow_invalid = allow_invalid
         self._row = 0
         self._ask = OrderedDict()
+        self._working_directory = os.getcwd()
 
         self._root = tk.Tk()
         self._root.title(self._title)
         
-        self._edited_entry = ttk.Style()
-        self._edited_entry.configure('edited.TEntry', foreground='black')
-        
-        self._unedited_entry = ttk.Style()
-        self._unedited_entry.configure('unedited.TEntry', foreground='#666')
+        data = encodebytes(get_data('ama', 'icon.gif'))
+        image = tk.PhotoImage(data=data)
+        self._root.tk.call('wm', 'iconphoto', self._root._w, image)
 
-        header_font = font.Font(family='TkDefaultFont', weight='bold')
+        f = font.Font(font=('TkHeadingFont', 0, font.BOLD))
+        ttk.Style().configure('header.TLabel', font=f)
+        
         header = ttk.Label(self._root, text=self._preamble, padding=3,
-                           font=header_font, wraplength=420)
+                           wraplength=420,
+                           style='header.TLabel')
         header.grid(column=0, row=0, sticky=(tk.N, tk.EW))
         
         self.content = ttk.Frame(self._root, padding=(3,3,3,3))
@@ -92,9 +89,9 @@ class TkAsker(Asker):
         
         # Swap the order of buttons for Windows
         if sys.platform.startswith('win32'):
-            btn_column = (1,2)
+            btn_column = (1, 2)
         else:
-            btn_column = (2,1)
+            btn_column = (2, 1)
             
         self.ok_btn = ttk.Button(okcancel, text='OK', width=10, command=self._ok)
         self.ok_btn.grid(column=btn_column[0], row=0, padx=(6, 0))
@@ -129,7 +126,10 @@ class TkAsker(Asker):
         """Perform the question asking by displaying in a Tkinter window"""
         
         self._result = {}
-        self.update_answers()
+        self._update_answers()
+        self._root.update_idletasks()
+        self._root.minsize(self._root.winfo_reqwidth(),
+                           self._root.winfo_reqheight())
         self._root.mainloop()
         return self._result
 
@@ -150,11 +150,11 @@ class TkAsker(Asker):
             
         return current_answers
     
-    def update_answers(self, update_info=None):
+    def _update_answers(self, update_info=None):
         """Update all unedited answers with the values from the other answers"""
         
         answers = self.current_answers(update_info)
-            
+
         for key, tkq in self._ask.items():
             if (update_info is None or key != update_info[0]) and \
             (not tkq.edited or key == update_info[0]):
@@ -209,44 +209,51 @@ class TkQuestion(object):
     def __init__(self, asker, row, question):
         self._key = question.key
         self._label = question.label
-        self._type = question.type
-        self._default = question.default
-        self._validator = question.validator
-        if self._validator and self._validator.startswith('path'):
-            self._default = os.path.normpath(self._default)
             
-        self._validate = asker.validator(self._type, self._validator)
+        self._type_validator = question.type_validator
+        self._custom_validator = question.custom_validator
+        self._validate = asker.validator(self._type_validator,
+                                         self._custom_validator)
+        
+        self._default = question.default
+        if self._custom_validator and self._custom_validator.startswith('path'):
+            self._default = os.path.normpath(self._default)
 
-        self._dont_update = ['date', 'time']
+        self._dont_update = ['date', 'time', 'color']
 
         self._var = None
         self._asker = asker
         self._row = row
         self._entry = None
+        self._entry_tooltip = None
         
         self._is_edited = False
         self._is_valid = True
 
-        self.label = ttk.Label(asker.content, text=self._label)
+        f = font.Font(font=('TkTextFont',))
+        ttk.Style().configure('question.TLabel', font=f)
+        
+        self.label = ttk.Label(asker.content, text=self._label,
+                               style='question.TLabel')
         self.label.grid(column=0, row=self._row, sticky=(tk.N, tk.S, tk.W),
                         padx=(0,5))
         
-        s = ttk.Style()
-        s.configure('error.TLabel', font='TkFixedFont 10 bold')
-        self.info_label = ttk.Label(asker.content, width=2,
-                                    anchor=tk.CENTER, style='error.TLabel')
-        self.info_label.grid(column=2, row=self._row, padx=(3,0))
+        ttk.Style().configure('error.TLabel', font=f)
+        self._info_label = ttk.Label(asker.content, width=2,
+                                    anchor=tk.CENTER)
+        self._info_label.grid(column=2, row=self._row, padx=(3,0))
+        
         self._help_text = question.help_text
         if self._help_text != '':
-            self.info_label['text'] = '?'
-            self.tooltip = ToolTip(self.info_label, msg=self._help_text, delay=0.5)
+            self._info_label['text'] = '?'
+            ToolTip(self._info_label, msg=self._help_text, delay=0.5)
         
-        self._validate_entry = (asker._root.register(self._tk_validate),
+        self._validate_entry = (asker._root.register(self._tk_validate_entry),
                                 '%P', '%V')
 
         current_answers = self._asker.current_answers()
 
-        if self._validator and self._validator.startswith('path'):
+        if self._custom_validator and self._custom_validator.startswith('path'):
             self._var = tk.StringVar()
             frame = self._create_entry_with_button(asker.content,
                 'Browse...',
@@ -254,15 +261,22 @@ class TkQuestion(object):
 
             self.update(current_answers)
             
-        elif self._validator and self._validator.startswith('date'):
-            frame = DateEntry(asker)
+        elif self._custom_validator and self._custom_validator.startswith('date'):
+            frame = DateEntry(asker.content)
+            self._entry = frame
             self._var = frame
             
-        elif self._validator and self._validator.startswith('time'):
-            frame = TimeEntry(asker)
+        elif self._custom_validator and self._custom_validator.startswith('time'):
+            frame = TimeEntry(asker.content)
+            self._entry = frame
             self._var = frame
             
-        elif self._type == 'str':
+        elif self._custom_validator and self._custom_validator.startswith('color'):
+            frame = ColorEntry(asker.content)
+            self._entry = frame
+            self._var = frame
+            
+        elif self._type_validator == 'str':
             self._var = tk.StringVar()
             self._entry = ttk.Entry(asker.content, textvariable=self._var,
                                    validate='all',
@@ -270,7 +284,7 @@ class TkQuestion(object):
             frame = self._entry
             self.update(current_answers)
             
-        elif self._type == 'int' or isinstance(self._type, int):
+        elif self._type_validator == 'int' or isinstance(self._type_validator, int):
             self._var = tk.IntVar()
             self._entry = ttk.Entry(asker.content, textvariable=self._var,
                                    validate='all',
@@ -280,7 +294,7 @@ class TkQuestion(object):
 
             self.update(current_answers)
             
-        elif self._type == 'float' or isinstance(self._type, float):
+        elif self._type_validator == 'float' or isinstance(self._type_validator, float):
             self._var = tk.DoubleVar()
             self._entry = ttk.Entry(asker.content, textvariable=self._var,
                                    validate='all',
@@ -290,13 +304,13 @@ class TkQuestion(object):
 
             self.update(current_answers)
             
-        elif self._type == 'bool' or self._type == 'yesno' or \
-            isinstance(self._type, bool):
+        elif self._type_validator == 'bool' or self._type_validator == 'yesno' or \
+            isinstance(self._type_validator, bool):
             self._var = tk.BooleanVar()
             self.value = self._default
             frame = ttk.Frame(asker.content)
             
-            if self._type == 'yesno':
+            if self._type_validator == 'yesno':
                 text = ('Yes', 'No')
             else:
                 text = ('True', 'False')
@@ -309,98 +323,91 @@ class TkQuestion(object):
                                 variable=self._var, value=False)
             n.grid(column=1, row=0)
 
-        elif isinstance(self._type, list):
+        elif isinstance(self._type_validator, list):
             self._var = tk.StringVar()
-            self.value = self._type[0]
-            if len(self._type) <= 3:
+            self.value = self._type_validator[0]
+            if len(self._type_validator) <= 3:
                 frame = ttk.Frame(asker.content)
-                for idx, e in enumerate(self._type):
+                for idx, e in enumerate(self._type_validator):
                     rb = ttk.Radiobutton(frame, text=str(e),
                                          variable=self._var, value=str(e))
                     rb.grid(column=idx, row=0, padx=(0,5))
             else:
                 self._entry = ttk.Combobox(asker.content,
                                           textvariable=self._var)
-                self._entry['values'] = tuple(self._type)
+                self._entry['values'] = tuple(self._type_validator)
                 frame = self._entry
             
         else:
-            raise ValueError(('Unable to create _entry widget '
-                              'for type %s') % self._type)
+            raise ValueError(('Unable to create entry widget '
+                              'for type %s') % self._type_validator)
             
         frame.grid(column=1, row=self._row, sticky=tk.EW, padx=0)
         
-        self.edited = self._is_edited
-        asker.content.rowconfigure(self._row, weight=0)
+        self.edited = False
+        asker.content.rowconfigure(self._row, weight=1)
         
     def update(self, current_answers):
         """Update our unedited value with the other answers."""
-        if self._validator is None:
+        if self._custom_validator is None:
             do_update = True
         else:
-            name = self._validator.split('(')[0]
+            name = self._custom_validator.split('(')[0]
             do_update =  name not in self._dont_update
             
         if do_update:
             updated_answer = str(self._default).format(**current_answers)
+            if self._custom_validator and self._custom_validator.startswith('path'):
+                self._update_path_tooltip(updated_answer)
             self.value = updated_answer
-        
-    def value():
-        def fget(self):
-            try:
-                return self._validate(self._var.get())
-            except:
-                return self._entry.get()
-        
-        def fset(self, value):
-            try:
-                value = self._validate(value)
-                self._var.set(value)
-                self.valid = True
-            except ValueError:
-                self._var.set(value)
-                self.valid = False
-                
-        return locals()
-    
-    value = property(**value())
+   
+    @property     
+    def value(self):
+        try:
+            return self._validate(self._var.get())
+        except:
+            return self._entry.value
 
-    def valid():
-        def fget(self):
-            return self._is_valid
-        
-        def fset(self, value):
-            self._is_valid = bool(value)
-            if self._is_valid:
-                if self._help_text != '':
-                    self.info_label['text'] = '?'
-                self.info_label['foreground'] = 'black'
+    @value.setter        
+    def value(self, value):
+        try:
+            value = self._validate(value)
+            self._var.set(value)
+            self.valid = True
+        except ValueError:
+            self._var.set(value)
+            self.valid = False
+
+    @property
+    def valid(self):
+        return self._is_valid
+
+    @valid.setter        
+    def valid(self, value):
+        self._is_valid = bool(value)
+        if self._is_valid:
+            if self._help_text != '':
+                self._info_label['text'] = '?'
+            self._info_label['background'] = ''
+        else:
+            self._info_label['text'] = '!'
+            self._info_label['background'] = '#e00'
+    
+    @property
+    def edited(self):
+        return self._is_edited
+
+    @edited.setter        
+    def edited(self, value):
+        self._is_edited = bool(value)
+
+        if isinstance(self._entry, ttk.Entry):
+            if self._is_edited:
+                self._entry['style'] = 'TEntry'
             else:
-                self.info_label['text'] = '!'
-                self.info_label['foreground'] = 'red'
-                
-        return locals()
+                self._entry['style'] = 'unedited.TEntry'
 
-    valid = property(**valid())
-        
-    def edited():
-        def fget(self):
-            return self._is_edited
-        
-        def fset(self, value):
-            self._is_edited = bool(value)
-            
-            if isinstance(self._entry, ttk.Entry):
-                if self._is_edited:
-                    self._entry['style'] = 'edited.TEntry'
-                else:
-                    self._entry['style'] = 'unedited.TEntry'
-                    
-        return locals()
-    
-    edited = property(**edited())
-    
-    def _tk_validate(self, P, V):
+    def _tk_validate_entry(self, P, V):
         rtn = 1
         if V == 'focusout':
             if P.strip() == '':
@@ -413,43 +420,43 @@ class TkQuestion(object):
                 except:
                     self.valid = False
                     rtn = 0
-                
+
             self._asker.check_invalid()
         elif V == 'key':
             if not self.edited:
                 self.edited = True
-            
-            self._asker.update_answers((self._key, P))
+
+            self._asker._update_answers((self._key, P))
 
         return rtn
 
     def _create_entry_with_button(self, master, text, command):
         frame = ttk.Frame(master)
-        
+
         self._entry = ttk.Entry(frame, textvariable=self._var, 
             validate='all', 
             validatecommand=self._validate_entry)
         self._entry.grid(column=0, row=0, sticky=tk.EW)
-        
+
         btn = ttk.Button(frame, text=text, 
             command=command)
-        btn.grid(column=1, row=0, sticky=tk.E)
-        
+        btn.grid(column=1, row=0, sticky=tk.E, padx=(6,0))
+
         frame.columnconfigure(0, weight=1)
         frame.columnconfigure(1, weight=0)
-        
+
         return frame
     
     def _browse_for_directory(self, *args):
         path_entry = self.value
         path_entry = os.path.abspath(os.path.expanduser(path_entry))
-        
+
         new_path = filedialog.askdirectory(initialdir=path_entry)
         new_path = os.path.normpath(new_path)
-        
+
         home_path = u(os.path.expanduser('~'))
         current_path = u(os.getcwd())
-        
+
         if new_path == home_path:
             new_path = '~'
         elif new_path.startswith(home_path):
@@ -458,6 +465,17 @@ class TkQuestion(object):
             new_path = '.'
         elif new_path.startswith(current_path):
             new_path = os.path.join('.', new_path[len(current_path)+1:])
-            
+
         self.value = new_path
-        self._asker.update_answers((self._key, new_path))
+        self._asker._update_answers((self._key, new_path))
+
+    def _update_path_tooltip(self, current_answer):
+        if self.edited:
+            value = self.value
+        else:
+            value = current_answer
+        
+        if not os.path.isabs(value):
+            value = os.path.abspath(os.path.join(os.getcwd(), value))
+        
+        self._entry_tooltip = ToolTip(self._entry, value, delay=0.5)
